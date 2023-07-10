9aa9ad8f2c6c6e3256406aa22713a0f57be32e88
tugce <tugcetopkaccerit@gmail.com>
2023-07-09 22:32:24 -0400

    trying to solve ERESOLVE issue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -47,7 +47,7 @@ const Contact = () => {
         initial={{ opacity: 0, y: -20 }}
         whileInView={{ opacity: 1 }}
         animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 2, delay: 0.2 }}
+        transition={{ duration: 2, delay: 0.5 }}
       >
         <form
           ref={form}
